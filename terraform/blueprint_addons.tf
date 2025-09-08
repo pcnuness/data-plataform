42120e3ee2af4f471f31216e96f747b851657576
Paulo César Nunes <paulo_cesar_nunes@hotmail.com>
2025-09-08 17:11:28 -0300

    Update blueprint_addons.tf

--- terraform/blueprint_addons.tf
+++ terraform/blueprint_addons.tf
@@ -72,8 +72,8 @@ module "eks_addons" {
     skip_crds     = false
     values = [
       templatefile("${path.root}/values/argocd.yaml.tftpl", {
-        url_gitops_argocd           = "gitops.654654197839.realhandsonlabs.net"
-        aws_acm_arn                 = "arn:aws:acm:us-east-1:654654197839:certificate/0739b5cb-ceda-4cd7-bee0-975793a95b34"
+        url_gitops_argocd           = "gitops.079910998645.realhandsonlabs.net"
+        aws_acm_arn                 = "arn:aws:acm:us-east-1:079910998645:certificate/6851a041-44d2-4257-9935-fbb6b904afd4"
         argocd_admin_password_mtime = null_resource.argocd_admin_password.triggers.modified
         argocd_admin_password       = null_resource.argocd_admin_password.triggers.hash
         aws_subnets_ids             = local.subnet_public_ids
