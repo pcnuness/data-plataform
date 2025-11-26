e6243748ca2e9e99f0a456e234507ca8d9aba017
Paulo César Nunes <paulo_cesar_nunes@hotmail.com>
2025-11-26 13:30:36 +0000

    feat: update argocd master account

--- terraform/blueprint_addons.tf
+++ terraform/blueprint_addons.tf
@@ -72,8 +72,8 @@ module "eks_addons" {
     skip_crds     = false
     values = [
       templatefile("${path.root}/values/argocd.yaml.tftpl", {
-        url_gitops_argocd           = "gitops.975049901454.realhandsonlabs.net"
-        aws_acm_arn                 = "arn:aws:acm:us-east-1:975049901454:certificate/2337b70d-9bc9-4bdf-afaa-21e421276d60"
+        url_gitops_argocd           = "gitops.694137446771.realhandsonlabs.net"
+        aws_acm_arn                 = "arn:aws:acm:us-east-1:694137446771:certificate/a95c06ac-bf27-4d6f-ae38-aff3296302fe"
         argocd_admin_password_mtime = null_resource.argocd_admin_password.triggers.modified
         argocd_admin_password       = null_resource.argocd_admin_password.triggers.hash
         aws_subnets_ids             = local.subnet_public_ids
